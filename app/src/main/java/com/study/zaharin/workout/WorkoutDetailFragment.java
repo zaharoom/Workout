0b104e1831a89cb791a52cb136b0480b656bc7f7
ZahARin <lavysh.zahar@gmail.com>
2016-12-17 18:24:27 +0200

    WorkoutDetail and WorkoutList Fragments were created

--- app/src/main/java/com/study/zaharin/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/study/zaharin/workout/WorkoutDetailFragment.java
@@ -6,14 +6,31 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class WorkoutDetailFragment extends Fragment {
 
+    private long workoutId;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        View v = getView();
+        if (v != null) {
+            TextView title = (TextView) v.findViewById(R.id.textTitle);
+            TextView description = (TextView) v.findViewById(R.id.textDescription);
+            Workout workout = Workout.workouts[(int)workoutId];
+            title.setText(workout.getName());
+            description.setText(workout.getDescription());
+        }
+    }
+
+    public void setWorkout(long workoutId) {
+        this.workoutId = workoutId;
+    }
 }
---
+++ app/src/main/java/com/study/zaharin/workout/WorkoutListFragment.java
@@ -0,0 +1,26 @@
+package com.study.zaharin.workout;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class WorkoutListFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        String[] names = new String[Workout.workouts.length];
+        for (int i = 0; i < names.length; i++) {
+            names[i] = Workout.workouts[i].getName();
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1, names);
+        setListAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
