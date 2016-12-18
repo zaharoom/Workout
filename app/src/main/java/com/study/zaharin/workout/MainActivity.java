475fbc54a4a23235a20b30c9a4429b3a59b1c851
ZahARin <lavysh.zahar@gmail.com>
2016-12-18 18:38:30 +0200

    Main app behavior was developed

--- app/src/main/java/com/study/zaharin/workout/MainActivity.java
+++ app/src/main/java/com/study/zaharin/workout/MainActivity.java
@@ -1,16 +1,28 @@
 package com.study.zaharin.workout;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity
+        implements WorkoutListFragment.WorkoutListListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        WorkoutDetailFragment frag = (WorkoutDetailFragment) getFragmentManager().findFragmentById(R.id.detail_frag);
-        frag.setWorkout(1);
+    @Override
+    public void onItemClicked(long id) {
+        WorkoutDetailFragment fragment = new WorkoutDetailFragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        fragment.setWorkout(id);
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        transaction.commit();
+
     }
 }
--- app/src/main/java/com/study/zaharin/workout/WorkoutListFragment.java
+++ app/src/main/java/com/study/zaharin/workout/WorkoutListFragment.java
@@ -1,18 +1,25 @@
 package com.study.zaharin.workout;
 
+import android.app.ListFragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
+import android.widget.ListView;
 
 public class WorkoutListFragment extends ListFragment {
 
+    static interface WorkoutListListener {
+        void onItemClicked(long id);
+    }
+
+    private WorkoutListListener listener;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        this.listener = (WorkoutListListener) getActivity();
 
         String[] names = new String[Workout.workouts.length];
         for (int i = 0; i < names.length; i++) {
@@ -23,4 +30,10 @@ public class WorkoutListFragment extends ListFragment {
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        if (listener != null) { //Activity attached?
+            listener.onItemClicked(id);
+        }
+    }
 }
